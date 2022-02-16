f8214396738143e7430df91fbd9b20682725e1c5
Francisco Sousa <fsousa.jpa@gmail.com>
2022-02-15 22:19:43 -0300

    Iniciando modificadores de acesso.

---
+++ src/ExerciciosFixacao/Exercicio_01/teste.java
@@ -0,0 +1,8 @@
+package ExerciciosFixacao.Exercicio_01;
+
+public class teste {
+
+    public static void main(String[] args) {
+        System.out.println("teste");
+    }
+}
---
+++ src/Topicos/Construtores/Program.java
@@ -0,0 +1,44 @@
+package Topicos.Construtores;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Product Data:");
+        System.out.print("Name: ");
+        String name = sc.nextLine(); //faz leitura do nome e guarda dentro dá variável product no campo name
+        System.out.print("Price: ");
+        double price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        int quantity = sc.nextInt();
+
+        Product product = new Product(name, price, quantity);
+
+        System.out.println();
+        System.out.println("Product data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be added in stock: ");
+        quantity = sc.nextInt();
+        product.addProducts(quantity); //Atualiza a quantidade de produtos dentro do objeto product
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        System.out.println();
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = sc.nextInt();
+        product.removeProducts(quantity);
+
+        System.out.println();
+        System.out.println("Updated data: " + product);
+
+        sc.close();
+    }
+}
