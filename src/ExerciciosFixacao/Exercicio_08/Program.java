029b18f3a97d8a0dd55f76886c1a2aba5d3b728a
Francisco <fsousa.jpa@gmail.com>
2022-02-23 16:47:09 -0300

    Iniciando tópico Date

--- src/ExerciciosFixacao/Exercicio_08/Program.java
+++ src/ExerciciosFixacao/Exercicio_08/Program.java
@@ -26,14 +26,26 @@ public class Program {
         int x = sc.nextInt();
         System.out.println();
 
-        for (int i = 0; i < ; i++) {
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
+                if (mat[i][j] == x) {
+                    System.out.println("Position " + i + "," + j);
 
+                    if (j - 1 >= 0 && mat[i][j - 1] != 0) {
+                        System.out.println("Left: " + mat[i][j - 1]);
+                    }
+                    if (j + 1 < c && mat[i][j + 1] != 0) {
+                        System.out.println("Right: " + mat[i][j + 1]);
+                    }
+                    if (i - 1 >= 0 && mat[i + 1][j] != 0) {
+                        System.out.println("Up: " + mat[i - 1][j]);
+                    }
+                    if (i + 1 < l && mat[i + 1][j] != 0) {
+                        System.out.println("Bottom: " + mat[i + 1][j]);
+                    }
+                }
+            }
         }
-
-
-
-
-
         sc.close();
     }
 }
