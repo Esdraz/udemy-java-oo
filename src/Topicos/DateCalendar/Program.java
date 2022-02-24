efbff8d8d5df707d98801e0b77dbc20535f36118
Francisco Sousa <fsousa.jpa@gmail.com>
2022-02-23 21:03:45 -0300

    Tópico Date e Calendar concluído

--- src/Topicos/DateCalendar/Program.java
+++ src/Topicos/DateCalendar/Program.java
@@ -1,4 +1,4 @@
-package Topicos.Date;
+package Topicos.DateCalendar;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -14,13 +14,19 @@ public class Program {
         SimpleDateFormat sdf2 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         SimpleDateFormat sdf3 = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         sdf3.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         Date x1 = new Date();
         Date x2 = new Date(System.currentTimeMillis());
         Date x3 = new Date(0L);
         Date x4 = new Date(1000L * 60L * 60L * 5L);
+
         Date y1 = sdf1.parse("25/06/2018");
         Date y2 = sdf2.parse("25/06/2018 15:42:07");
         Date y3 = Date.from(Instant.parse("2018-06-25T15:42:07Z"));
+
+        System.out.println("-------------");
+        System.out.println("Sem formatar:");
+        System.out.println("-------------");
         System.out.println("x1: " + x1);
         System.out.println("x2: " + x2);
         System.out.println("x3: " + x3);
@@ -29,6 +35,8 @@ public class Program {
         System.out.println("y2: " + y2);
         System.out.println("y3: " + y3);
         System.out.println("-------------");
+        System.out.println("Formatado no formato UTC:");
+        System.out.println("-------------");
         System.out.println("x1: " + sdf2.format(x1));
         System.out.println("x2: " + sdf2.format(x2));
         System.out.println("x3: " + sdf2.format(x3));
@@ -36,6 +44,8 @@ public class Program {
         System.out.println("y1: " + sdf2.format(y1));
         System.out.println("y2: " + sdf2.format(y2));
         System.out.println("y3: " + sdf2.format(y3));
+        System.out.println("-------------");
+        System.out.println("Formatado no formato GMT:");
         System.out.println("-------------");
         System.out.println("x1: " + sdf3.format(x1));
         System.out.println("x2: " + sdf3.format(x2));
